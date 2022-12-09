3d0706750378aa7e9859e2e7e01756def55fca0a
loctp2003 <93903317+loctp2003@users.noreply.github.com>
2022-12-09 23:11:07 +0700

    Update 🆙LinearRegression.py

--- pages/🆙LinearRegression.py
+++ pages/🆙LinearRegression.py
@@ -27,8 +27,8 @@ def Bai01():
         y2 = x2*w[1, 0] + w[0, 0]
         plt.plot(X, y.T, 'ro')
         plt.plot([x1, x2], [y1, y2])
-     
         st.pyplot()
+        st.set_option('deprecation.showPyplotGlobalUse', False)
     with col2:
         st.write("w :",w)
         #Tinh sai so
